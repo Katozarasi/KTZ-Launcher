0013e96f167fb7a6e870924be6c13d6581a12a2f
KatoDayo <137963618+Katozarasi@users.noreply.github.com>
2026-06-05 13:35:53 +0900

    Switch NeoForge artifact fixer to universal jar

--- tools/fix-neoforge-client-artifact.js
+++ tools/fix-neoforge-client-artifact.js
@@ -9,11 +9,11 @@ const loader = server.modules.find(m => m.id === 'net.neoforged:neoforge:21.4.15
 if (!loader) throw new Error('NeoForge loader not found. Run npm run generate:server kato_empire_test first.')
 
 loader.artifact = {
-  size: 5891137,
-  MD5: '70505efad05655c750012855fe3b0c7f',
-  path: 'net/neoforged/neoforge/21.4.157/neoforge-21.4.157-client.jar',
-  url: 'https://raw.githubusercontent.com/Katozarasi/KTZ-Launcher/main/files/loaders/neoforge/21.4.157/neoforge-21.4.157-client.jar'
+  size: 3513954,
+  MD5: 'cf63fa31dff33454624a0ec4c5a62cee',
+  path: 'net/neoforged/neoforge/21.4.157/neoforge-21.4.157-universal.jar',
+  url: 'https://raw.githubusercontent.com/Katozarasi/KTZ-Launcher/main/files/loaders/neoforge/21.4.157/neoforge-21.4.157-universal.jar'
 }
 
 fs.writeFileSync(distroPath, JSON.stringify(distro, null, 2) + '\n', 'utf8')
-console.log('NeoForge client artifact applied to kato_empire_test')
+console.log('NeoForge universal artifact applied to kato_empire_test')

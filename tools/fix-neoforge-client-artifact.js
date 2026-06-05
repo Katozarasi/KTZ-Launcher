da710d4fac7492790f36f76b20034a6ce05d2a9f
KatoDayo <137963618+Katozarasi@users.noreply.github.com>
2026-06-05 14:49:05 +0900

    Use generated NeoForge version jar as loader artifact

--- tools/fix-neoforge-client-artifact.js
+++ tools/fix-neoforge-client-artifact.js
@@ -9,11 +9,13 @@ const loader = server.modules.find(m => m.id === 'net.neoforged:neoforge:21.4.15
 if (!loader) throw new Error('NeoForge loader not found. Run npm run generate:server kato_empire_test first.')
 
 loader.artifact = {
-  size: 3513954,
-  MD5: 'cf63fa31dff33454624a0ec4c5a62cee',
-  path: 'net/neoforged/neoforge/21.4.157/neoforge-21.4.157-universal.jar',
-  url: 'https://raw.githubusercontent.com/Katozarasi/KTZ-Launcher/main/files/loaders/neoforge/21.4.157/neoforge-21.4.157-universal.jar'
+  size: 28335587,
+  MD5: '70e2838411853210dce14bdd30769458',
+  path: 'net/neoforged/neoforge/21.4.157/neoforge-21.4.157.jar',
+  url: 'https://raw.githubusercontent.com/Katozarasi/KTZ-Launcher/main/files/loaders/neoforge/21.4.157/neoforge-21.4.157.jar'
 }
 
+loader.subModules = (loader.subModules || []).filter(m => m.id !== 'net.neoforged:neoforge:21.4.157:client')
+
 fs.writeFileSync(distroPath, JSON.stringify(distro, null, 2) + '\n', 'utf8')
-console.log('NeoForge universal artifact applied to kato_empire_test')
+console.log('NeoForge generated version jar applied to kato_empire_test')
